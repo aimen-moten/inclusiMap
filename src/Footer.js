b6fc320a3d9f5963b5cea79aaf55db093eb57d9f
Aimen Moten <aimen.moten@gmail.com>
2023-06-25 20:28:08 +0500

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='text-white bg-oliveGreen text-lg p-4 text-center'>
-        Made for Waffle Hacks 2023 by Team X
+        Made for Waffle Hacks 2023 by Aimen, Anna & Nazmul
     </footer>
   )
 }
